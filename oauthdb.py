e2cb9328bd2406813722fed67e48c475d87a8e66
Jacob Adams <jacob@tookmund.com>
2019-05-11 17:20:32 -0400

    oauthdb: Import User and format imports

--- oauthdb.py
+++ oauthdb.py
@@ -1,7 +1,9 @@
 from sqlalchemy import Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship
-from sql import Base
 from flask_dance.consumer.storage.sqla import OAuthConsumerMixin, SQLAlchemyStorage
+
+from sql import Base
+from models import User
 
 class OAuth(OAuthConsumerMixin, Base):
     user_id = Column(Integer, ForeignKey(User.id))
