6d591856c792f08c2767e07d6f3b7ff841eff47b
Jacob Adams <jacob@tookmund.com>
2019-05-11 17:14:41 -0400

    models: Add basic sql tables

--- models.py
+++ models.py
@@ -1,2 +1,16 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Boolean, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from sql import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    email = Column(String)
+    name = Column(String)
+
+class HW(Base):
+    __tablename__ = "HW"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    quantity = Column(Integer)
+    whom = Column(String)
