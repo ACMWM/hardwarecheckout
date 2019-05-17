f813e582e332264ac3cb040a7b0ac3cd79685ba6
Jacob Adams <jacob@tookmund.com>
2019-05-17 18:07:23 -0400

    Add very basic addhw script

---
+++ addhw.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import sys
+
+import sql
+import models
+
+sql.init_db()
+
+if len(sys.argv) > 2:
+    q = int(sys.argv[2])
+else:
+    q = 1
+h = models.HW(name=sys.argv[1], quantity=q)
+
+sql.add(h)
+sql.commit()
