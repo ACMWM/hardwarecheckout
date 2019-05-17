5e702f27d65315be95398267bc1b6dc6f29dca5b
Jacob Adams <jacob@tookmund.com>
2019-05-17 18:05:29 -0400

    adduser: More verbose

--- adduser.py
+++ adduser.py
@@ -4,6 +4,10 @@ import sys
 import sql
 from models import User
 
+if len(sys.argv) != 2:
+    print("Usage: "+sys.argv[0]+" <email>")
+    sys.exit(1)
+
 sql.init_db()
 input("Are you sure you want to add "+sys.argv[1]+"? Press Ctrl+C to cancel,"
         "or anything else to continue")
@@ -12,3 +16,4 @@ input("Are you sure you want to add "+sys.argv[1]+"? Press Ctrl+C to cancel,"
 u = User(email=sys.argv[1])
 sql.add(u)
 sql.commit()
+print("Added "+sys.argv[1]+" to the authorized users list.")
