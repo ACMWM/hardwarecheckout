58c487d4fff8d119ec917127268e4a79cad730e9
Jacob Adams <jacob@tookmund.com>
2019-05-22 15:06:22 -0400

    auth: Add email regex
    https://emailregex.com/

--- auth.py
+++ auth.py
@@ -1,7 +1,13 @@
 from flask import session
+from re import compile
 import sql
 
 domain = "email.wm.edu"
 
 def checkemail(email):
     return sql.checkemail()
+
+_email = compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+
+def validemail(e):
+    return _email.fullmatch(e)
