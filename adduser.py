22ecca085430d5a222d3b51f241a0b25956c43b3
Jacob Adams <jacob@tookmund.com>
2019-05-22 15:39:00 -0400

    adduser: Use auth to add domain and check email

--- adduser.py
+++ adduser.py
@@ -2,18 +2,24 @@
 import sys
 
 import sql
+import auth
 from models import User
 
 if len(sys.argv) != 2:
-    print("Usage: "+sys.argv[0]+" <email>")
+    print("Usage: "+sys.argv[0]+" <username>")
     sys.exit(1)
 
 sql.init_db()
-input("Are you sure you want to add "+sys.argv[1]+"? Press Ctrl+C to cancel,"
+email = sys.argv[1]+"@"+auth.domain
+if validemail(email) is None:
+    print(email+" is not a valid email.")
+    sys.exit(2)
+
+input("Are you sure you want to add "+email+"? Press Ctrl+C to cancel,"
         "or anything else to continue")
 
 
 u = User(email=sys.argv[1])
 sql.add(u)
 sql.commit()
-print("Added "+sys.argv[1]+" to the authorized users list.")
+print("Added "+email+" to the authorized users list.")
