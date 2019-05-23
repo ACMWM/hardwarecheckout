7109d0cc2743378dc11df00c788dc26b8c35ccd2
Jacob Adams <jacob@tookmund.com>
2019-05-23 13:19:35 -0400

    login: Import logout_user function

--- login.py
+++ login.py
@@ -1,4 +1,4 @@
-from flask_login import LoginManager, current_user, login_user
+from flask_login import LoginManager, current_user, login_user, logout_user
 from sql import getuser
 
 manager = LoginManager()
