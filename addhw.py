6d1231b8920b56d241cf20d94bd71f12a516c385
Jacob Adams <jacob@tookmund.com>
2019-05-23 17:51:35 -0400

    addhw.py: Add support for setting category for testing

--- addhw.py
+++ addhw.py
@@ -7,10 +7,14 @@ import models
 
 sql.init_db()
 
+if len(sys.argv) > 3:
+    c = sys.argv[3]
+else:
+    c = None
 if len(sys.argv) > 2:
     q = int(sys.argv[2])
 else:
     q = 1
-h = models.HW(name=sys.argv[1], quantity=q, available=q)
+h = models.HW(name=sys.argv[1], quantity=q, available=q, category=c)
 
 sql.add(h)
