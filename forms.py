fca8173be56a1130b9bf33453d99150dd5772407
Jacob Adams <jacob@tookmund.com>
2019-05-24 13:03:31 -0400

    Revert "Simplify deluser by inheriting from newuser"
    This reverts commit 6907a94857642497bd5979c5aa2583ed5d515a50.

--- forms.py
+++ forms.py
@@ -74,5 +74,5 @@ class NewUser(FlaskForm):
         if auth.validemail(field.data) is None:
             raise ValidationError("Must be a valid email!")
 
-class DelUser(NewUser):
+class DelUser(FlaskForm):
     email = SelectField("User")
