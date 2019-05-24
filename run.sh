4121164377ac1e265c43d5eecf7e8b5fa6b57c33
Jacob Adams <jacob@tookmund.com>
2019-05-24 16:12:22 -0400

    Split requirement installation and running program

--- run.sh
+++ run.sh
@@ -4,5 +4,4 @@ cd "$(dirname "$0")"
 . bin/activate
 . ./.env
 
-pip install -r requirements.txt
 exec gunicorn main:app --log-file=- --access-logfile=- --capture-output
---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+virtualenv -p /usr/bin/python3 .
+. bin/activate
+pip install -r requirements.txt
